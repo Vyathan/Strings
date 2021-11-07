88bafb426290ab8c11672c42de0e175188927bc2
Vyathan <mattjbrundage@icloud.com>
2021-11-06 21:22:13 -0400

    Complete

--- StringReverse.cpp
+++ StringReverse.cpp
@@ -11,5 +11,18 @@ using namespace std;
 
 int main()
 {
+    // Declare
+    string characters = "";
+
+    // Accept user input
+    cout << "Enter a string: ";
+    cin >> characters;
+
+    // Reverse the order
+    int x = strlen(characters);
+    cout << "The string reversed is: ";
+    for (int y = x; y >= 0; y--)
     
-}
+    // Display results
+    cout << characters[y];
+}// End program
